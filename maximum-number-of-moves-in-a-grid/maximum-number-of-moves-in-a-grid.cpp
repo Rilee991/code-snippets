fc273f9dc330983e6fb7a27fb975136d9ed68bee
CiPHeR33 <rohit.kerketta@byjus.com>
2024-10-29 20:31:34 +0530

    feat: update cpp solution for maximum-number-of-moves-in-a-grid

--- maximum-number-of-moves-in-a-grid/maximum-number-of-moves-in-a-grid.cpp
+++ maximum-number-of-moves-in-a-grid/maximum-number-of-moves-in-a-grid.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    bool isValid(int row, int col, vector<vector<bool>> &ways, vector<vector<int>>& grid) {
-        if(row-1 >= 0 && ways[row-1][col-1] && grid[row-1][col-1] < grid[row][col])
+    bool isValid(int row, int col, vector<vector<int>>& grid) {
+        if(row-1 >= 0 && grid[row-1][col-1] < grid[row][col])
             return true;
-        if(row >= 0 && ways[row][col-1] && grid[row][col-1] < grid[row][col])
+        if(row >= 0 && grid[row][col-1] < grid[row][col])
             return true;
-        if(row+1 < ways.size() && ways[row+1][col-1] && grid[row+1][col-1] < grid[row][col])
+        if(row+1 < grid.size() && grid[row+1][col-1] < grid[row][col])
             return true;
         
         return false;
@@ -13,13 +13,13 @@ public:
 
     int maxMoves(vector<vector<int>>& grid) {
         int n = grid.size(), m = grid[0].size();
-        vector<vector<bool>> ways(n, vector<bool>(m, true));
+        // vector<vector<bool>> ways(n, vector<bool>(m, true));
         int res = 0;
 
         for(int col=1;col<m;col++) {
             for(int row=0;row<n;row++) {
-                if(!isValid(row, col, ways, grid)) {
-                    ways[row][col] = false;
+                if(!isValid(row, col, grid)) {
+                    grid[row][col] = INT_MAX;
                 } else {
                     res = max(res, col);
                 }
