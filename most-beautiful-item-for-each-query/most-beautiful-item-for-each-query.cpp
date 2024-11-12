85d04af3691bb10eda40b85410c610d97d74f6ad
CiPHeR33 <rohit.kerketta@byjus.com>
2024-11-12 23:14:06 +0530

    feat: update cpp solution for most-beautiful-item-for-each-query

--- most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.cpp
+++ most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.cpp
@@ -1,37 +1,24 @@
 class Solution {
 public:
     vector<int> maximumBeauty(vector<vector<int>>& items, vector<int>& queries) {
-        unordered_map<int,int> mp;
         vector<int> res;
 
-        for(vector<int> &item: items) {
-            int price = item[0];
-            int beauty = item[1];
+        sort(items.begin(), items.end());
+        int maxBeauty = items[0][1];
 
-            mp[price] = max(mp[price], beauty);
-        }
-
-        vector<vector<int>> uniqItems;
-
-        for(auto it: mp)
-            uniqItems.push_back({ it.first, it.second });
-        
-        sort(uniqItems.begin(), uniqItems.end());
-        int maxBeauty = uniqItems[0][1];
-
-        for(int i=1;i<uniqItems.size();i++) {
-            maxBeauty = max(maxBeauty, uniqItems[i][1]);
-            uniqItems[i][1] = maxBeauty;
+        for(int i=1;i<items.size();i++) {
+            maxBeauty = max(maxBeauty, items[i][1]);
+            items[i][1] = maxBeauty;
         }
 
         for(int query: queries) {
-            int lo = 0, hi = uniqItems.size()-1;
+            int lo = 0, hi = items.size()-1;
             int index = -1;
 
             while(lo <= hi) {
                 int mid = (lo+hi)/2;
 
-                if(uniqItems[mid][0] <= query) {
+                if(items[mid][0] <= query) {
                     index = mid;
                     lo = mid+1;
                 } else {
@@ -39,7 +26,7 @@ public:
                 }
             }
             
-            res.push_back(index != -1 ? uniqItems[index][1] : 0);
+            res.push_back(index != -1 ? items[index][1] : 0);
         }
 
         return res;
