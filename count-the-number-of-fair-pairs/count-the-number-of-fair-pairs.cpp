271ec3ee2ee629f0bfa996166a9ed76ad05b59fa
CiPHeR33 <rohit.kerketta@byjus.com>
2024-11-13 16:58:41 +0530

    feat: update cpp solution for count-the-number-of-fair-pairs

--- count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
+++ count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
-    int getLowerLimit(vector<int> &nums, int limit, int lo) {
+    int getLowerLimit(vector<int> &nums, int limit, int lo, char type) {
         int hi = nums.size() - 1;
-        int index = 1e6;
+        int index = (type == 'L' ? 1e6 : -1);
 
         while(lo <= hi) {
             int mid = (lo+hi)/2;
 
-            if(nums[mid] >= limit) {
-                index = mid;
+            if(nums[mid] > limit) {
+                if(type == 'L') index = mid;
                 hi = mid - 1;
+            } else if(nums[mid] == limit) {
+                index = mid;
+                if(type == 'L') hi = mid - 1;
+                else lo = mid + 1;
             } else {
+                if(type != 'L') index = mid;
                 lo = mid + 1;
             }
         }
@@ -42,8 +47,8 @@ public:
         sort(nums.begin(), nums.end());
 
         for(int i=0;i<nums.size();i++) {
-            int lowLimit = getLowerLimit(nums, lower - nums[i], i+1);
-            int upLimit = getUpperLimit(nums, upper - nums[i], i+1);
+            int lowLimit = getLowerLimit(nums, lower - nums[i], i+1, 'L');
+            int upLimit = getLowerLimit(nums, upper - nums[i], i+1, 'U');
             // cout<<"num:"<<nums[i]<<", lolim:"<<lowLimit<<", upLim:"<<upLimit<<"\n";
 
             res += max(0, (upLimit - lowLimit + 1));
