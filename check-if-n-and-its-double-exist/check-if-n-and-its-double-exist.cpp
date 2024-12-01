794158eeecbe2a1e03959f8b1603c6e371d4272d
CiPHeR33 <rohit.kerketta@byjus.com>
2024-12-01 15:34:19 +0530

    feat: add cpp solution for check-if-n-and-its-double-exist

---
+++ check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        unordered_map<int,bool> mp;
+
+        for(int num: arr) {
+            if(mp.find(num * 2) != mp.end())
+                return true;
+            if(num%2 == 0 && mp.find(num/2) != mp.end())
+                return true;
+            
+            mp[num] = 1;
+        }
+
+        return false;
+    }
+};
