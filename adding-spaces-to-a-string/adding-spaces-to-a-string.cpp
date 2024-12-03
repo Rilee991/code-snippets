db519b7f23a07ca5c52e1b235b5443dbb9d0be98
CiPHeR33 <rohit.kerketta@byjus.com>
2024-12-03 19:50:19 +0530

    feat: add cpp solution for adding-spaces-to-a-string

---
+++ adding-spaces-to-a-string/adding-spaces-to-a-string.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string addSpaces(string s, vector<int>& spaces) {
+        string res = "";
+        int spaceIdx = 0;
+
+        sort(spaces.begin(), spaces.end());
+
+        for(int i=0;i<s.size();i++) {
+            if(spaceIdx < spaces.size() && i == spaces[spaceIdx]) {
+                res += ' ';
+                spaceIdx++;
+            }
+
+            res += s[i];
+        }
+
+        return res;
+    }
+};
