c0ff27e3d93ac9ba684716eae7ab7d4c32bed24f
CiPHeR33 <rohit.kerketta@byjus.com>
2024-12-23 23:24:22 +0530

    feat: add cpp solution for minimum-number-of-operations-to-sort-a-binary-tree-by-level

---
+++ minimum-number-of-operations-to-sort-a-binary-tree-by-level/minimum-number-of-operations-to-sort-a-binary-tree-by-level.cpp
@@ -0,0 +1,64 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int findMinSwap(vector<int> &arr) {
+        unordered_map<int,int> elemIdxMap;
+        vector<int> sortedArr(arr.begin(), arr.end());
+        int res = 0;
+
+        sort(sortedArr.begin(), sortedArr.end());
+
+        for(int i=0;i<arr.size();i++) {
+            elemIdxMap[arr[i]] = i;
+        }
+
+        for(int i=0;i<arr.size();i++) {
+            if(arr[i] != sortedArr[i]) {
+                int sortedIdx = elemIdxMap[sortedArr[i]];
+                elemIdxMap[arr[i]] = sortedIdx;
+                elemIdxMap[arr[sortedIdx]] = i;
+                swap(arr[i], arr[sortedIdx]);
+                res++;
+            }
+        }
+
+        return res;
+    }
+
+    int minimumOperations(TreeNode* root) {
+        queue<TreeNode*> q;
+        int res = 0;
+
+        q.push(root);
+
+        while(!q.empty()) {
+            int size = q.size();
+            vector<int> arr(size);
+
+            for(int i=0;i<size;i++) {
+                arr[i] = q.front()->val;
+
+                if(q.front()->left)
+                    q.push(q.front()->left);
+                
+                if(q.front()->right)
+                    q.push(q.front()->right);
+                q.pop();
+            }
+
+            res += findMinSwap(arr);
+        }
+
+        return res;
+    }
+};
