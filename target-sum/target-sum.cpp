a1eb76be0796c5ddeebddaf20999abdbc944682b
CiPHeR33 <rohit.kerketta@byjus.com>
2024-12-26 09:50:32 +0530

    feat: update cpp solution for target-sum

--- target-sum/target-sum.cpp
+++ target-sum/target-sum.cpp
@@ -1,17 +1,22 @@
 class Solution {
+    vector<unordered_map<int,int>> dp;
 public:
     int dfs(vector<int> &nums, int n, int target) {
         if(n == 0) {
             return target == 0;
         }
 
+        if(dp[n].find(target) != dp[n].end())
+            return dp[n][target];
+
         int add = dfs(nums, n-1, target-nums[n-1]);
         int sub = dfs(nums, n-1, target+nums[n-1]);
 
-        return add + sub;
+        return dp[n][target] = add + sub;
     }
 
     int findTargetSumWays(vector<int>& nums, int target) {
+        dp.resize(nums.size()+1);
         return dfs(nums, nums.size(), target);
     }
 };
