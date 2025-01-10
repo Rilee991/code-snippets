69d0dead18c3ba7cf7e683237b6273134bf01ed8
CiPHeR33 <rohit.kerketta@byjus.com>
2025-01-10 23:25:34 +0530

    feat: update cpp solution for word-subsets

--- word-subsets/word-subsets.cpp
+++ word-subsets/word-subsets.cpp
@@ -3,29 +3,29 @@ public:
     vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
         // a1b1c1e1f1k1o2, a1e1l1p2, a2m1n1o1z1, c1d1e3l1o1t1, e1g2l1o2
         // e1, o1
-        unordered_map<char,int> mp2;
+        vector<int> mp2(26, 0);
         vector<string> res;
 
         for(string &word: words2) {
-            unordered_map<char,int> counter;
+            vector<int> counter(26, 0);
             for(char ch: word) {
-                counter[ch]++;
+                counter[ch-'a']++;
 
-                if(mp2[ch] < counter[ch])
-                    mp2[ch] = counter[ch];
+                if(mp2[ch-'a'] < counter[ch-'a'])
+                    mp2[ch-'a'] = counter[ch-'a'];
             }
         }
 
         for(string &word: words1) {
-            unordered_map<char,int> counter;
+            vector<int> counter(26, 0);
             for(char ch: word) {
-                counter[ch]++;
+                counter[ch-'a']++;
             }
 
             bool flag = true;
 
-            for(auto it: mp2) {
-                if(counter[it.first] < it.second) {
+            for(int i=0;i<26;i++) {
+                if(counter[i] < mp2[i]) {
                     flag = false;
                     break;
                 }
