00494dd44b5fc571b7998d99ab49d14ced2abd96
CiPHeR33 <rohit.kerketta@byjus.com>
2025-01-16 14:25:45 +0530

    feat: update cpp solution for bitwise-xor-of-all-pairings

--- bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.cpp
+++ bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.cpp
@@ -5,15 +5,15 @@ public:
 
         if((nums1.size()&1) && (nums2.size()&1)) {
             for(int i=0;i<nums1.size();i++)
-                res = res ^ nums1[i];
+                res ^= nums1[i];
             for(int i=0;i<nums2.size();i++)
-                res = res ^ nums2[i];
+                res ^= nums2[i];
         } else if(nums1.size()&1) {
             for(int i=0;i<nums2.size();i++)
-                res = res ^ nums2[i];
+                res ^= nums2[i];
         } else if(nums2.size()&1) {
             for(int i=0;i<nums1.size();i++)
-                res = res ^ nums1[i];
+                res ^= nums1[i];
         }
 
         return res;
